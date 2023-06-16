dd06c67aa6a105906fc66f61d2f69f630050c48d
angelili <99340194+angelili@users.noreply.github.com>
2023-06-16 12:28:46 +0200

    Update client.py

--- client.py
+++ client.py
@@ -16,6 +16,43 @@ import mnist
 
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+ def objective_function(local_model, global_model, lambda_reg, data, target):
+    output = local_model(data)
+    loss = F.cross_entropy(output, target)
+
+    local_params = torch.cat([param.view(-1) for param in local_model.parameters()])
+    global_params = torch.cat([param.view(-1) for param in global_model.parameters()])
+
+    proba=torch.norm(local_params - global_params)**2
+
+    proba_leaf = torch.tensor(proba, requires_grad=True)
+    objective = loss + (lambda_reg/2) * proba_leaf
+
+    return objective, loss, output, target
+def gradient_norm_stop_callback(threshold=1e-5):
+    """
+    Callback function to monitor the optimization process and stop it once the gradient norm falls below a certain
+    threshold.
+
+    Args:
+        threshold (float): Gradient norm threshold. Default is 1e-5.
+    """
+
+    def callback_function(optimizer):
+        gradient_norm = 0.0
+        for group in optimizer.param_groups:
+          
+            for param in group['params']:
+                
+                if isinstance(param.grad, torch.Tensor):
+                    gradient_norm += torch.norm(param.grad)**2
+        gradient_norm = gradient_norm.sqrt().item()
+        if gradient_norm < threshold:
+            print(f'Gradient norm ({gradient_norm:.6f}) is below the threshold ({threshold:.6f}). Stopping optimization.')
+            return True
+
+    return callback_function
+
 
 # Flower Client
 class CifarClient(fl.client.NumPyClient):
@@ -47,16 +84,17 @@ class CifarClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-
+        # Define the personalized objective function using the Moreau envelope algorithm
+       
         local_model=copy.deepcopy(self.model).to(DEVICE)
         local_model.train()
         for r in range(2):
             correct, total, epoch_loss = 0, 0, 0.0
             # Local update on client 
-            optimizer = torch.optim.SGD(local_model.parameters(), lr=learning_rate)
+            optimizer = torch.optim.SGD(local_model.parameters(), lr=0.1)
             for batch_idx, (data, target) in enumerate(self.trainloader):
                 optimizer.zero_grad()
-                objective, loss, output, target = objective_function(local_model, self.model, lambda_reg, data.to(DEVICE), target.to(DEVICE))
+                objective, loss, output, target = objective_function(local_model, self.model, 15, data.to(DEVICE), target.to(DEVICE))
                 #objective.requires_grad = True
                 objective.backward()
                 optimizer.step()
@@ -72,7 +110,7 @@ class CifarClient(fl.client.NumPyClient):
             # Compute Moreau envelope of local model
             with torch.no_grad():
               for param, global_param in zip(local_model.parameters(), self.model.parameters()):
-                  global_param.data=global_param.data-eta*lambda_reg*(global_param.data-param.data)
+                  global_param.data=global_param.data-0.05*15*(global_param.data-param.data)
 
             epoch_loss /= len(self.trainloader.dataset)
             epoch_acc = correct / total
