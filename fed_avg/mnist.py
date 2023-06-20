bd027717aab9424428deb192f5a8ff99068d4911
angelili <99340194+angelili@users.noreply.github.com>
2023-06-20 18:06:00 +0200

    Update mnist.py

--- fed_avg/mnist.py
+++ fed_avg/mnist.py
@@ -15,8 +15,8 @@ import numpy as np
 
 
 DATA_ROOT = "./dataset"
-Dominant_class=True
-Non_uniform_cardinality=False
+Dominant_class=False
+Non_uniform_cardinality=True
 
 def load_data() -> (
     Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, Dict]):
